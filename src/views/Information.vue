768e54d5b9d62443f1c927400848fa2923425510
Your Name <you@example.com>
2022-01-17 17:14:46 +0800

    haha

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -40,7 +40,7 @@ import TopHeader from "../components/Top-Header";
                 this.button = false;
             },
             changePassword(){
-                // check haha
+                // check haha OO
                 var user = firebase.auth().currentUser;
                 if((this.password_1 === this.password_2) && (this.password_1.length>=6) && (this.password_2.length>=6)){
                     user.updatePassword(this.password_2);
